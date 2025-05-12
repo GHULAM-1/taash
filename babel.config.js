be3fbb8389aca845a212088db268e61d2bb50189
GHULAM-1 <ilyasghulam35@gmail.com>
2025-05-12 11:12:21 +0500

    clear the main branch

--- babel.config.js
+++
@@ -1,9 +0,0 @@
-// babel.config.js
-module.exports = function (api) {
-    api.cache(true);
-    return {
-      presets: ['babel-preset-expo'],            // includes Expo-Router support
-      plugins: ['react-native-reanimated/plugin'],// compiles JSI glue
-    };
-  };
-  
